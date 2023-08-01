343c58d06961f986aecfdacaeb217c4f2e0d672f
junseokoh-hub <lunulatecat@naver.com>
2023-08-02 00:03:58 +0900

    Feat: usedebounce hooks with searching lists

--- src/components/page-management/PMTableBody.jsx
+++ src/components/page-management/PMTableBody.jsx
@@ -5,24 +5,30 @@ const PMTableBody = ({ pageLists, offset }) => {
 	const navigate = useNavigate();
 	return (
 		<tbody>
-			{pageLists.slice(offset, offset + 10).map((list) => (
-				<tr key={list.title}>
-					<td>
-						<input type="checkbox" name="checkbox" />
-					</td>
-					<td>{list.title}</td>
-					<td>{list.link}</td>
-					<td>{list.key}</td>
-					<td>{new Date().toLocaleString()}</td>
-					<td>
-						<button type="button">상세</button>
-						<button type="button">복제</button>
-						<button type="button" onClick={() => navigate('/admin/edit')}>
-							디자인
-						</button>
-					</td>
+			{pageLists.length > 0 ? (
+				pageLists.slice(offset, offset + 10).map((list) => (
+					<tr key={list.title}>
+						<td>
+							<input type="checkbox" name="checkbox" />
+						</td>
+						<td>{list.title}</td>
+						<td>{list.link}</td>
+						<td>{list.key}</td>
+						<td>{new Date().toLocaleString()}</td>
+						<td>
+							<button type="button">상세</button>
+							<button type="button">복제</button>
+							<button type="button" onClick={() => navigate('/admin/edit')}>
+								디자인
+							</button>
+						</td>
+					</tr>
+				))
+			) : (
+				<tr>
+					<td>no data</td>
 				</tr>
-			))}
+			)}
 		</tbody>
 	);
 };
--- src/components/page-management/PageManagement.jsx
+++ src/components/page-management/PageManagement.jsx
@@ -1,20 +1,21 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
+import { useDebounce } from '../../hooks/useDebounce';
 import { menuLists } from '../../libs/menu-lists';
 import PMTable from './PMTable';
 import PMTableHead from './PMTableHead';
 import PMTableBody from './PMTableBody';
 import PMPaignation from './PMPaignation';
-
-const pageLists = Object.entries(menuLists)
-	.map(([key, value]) => {
-		value.forEach((item) => (item['key'] = key));
-		return value;
-	})
-	.reduce((acc, cur) => acc.concat(cur));
+import PMSearch from './PMSearch';
 
 const PageManagement = () => {
+	const [search, setSearch] = useState('');
+	const debouncedValue = useDebounce(search, 500);
 	const [currentPage, setCurrentPage] = useState(1);
 	const offset = (currentPage - 1) * 10;
+
+	const changeInputHandler = useCallback((e) => {
+		setSearch(e.target.value);
+	}, []);
 
 	const changePageHandler = useCallback((e) => {
 		const { name } = e.target;
@@ -28,8 +29,21 @@ const PageManagement = () => {
 		}
 	}, []);
 
+	const pageLists = useMemo(() => {
+		return Object.entries(menuLists)
+			.map(([key, value]) => {
+				value.forEach((item) => (item['key'] = key));
+				return value;
+			})
+			.reduce((acc, cur) => acc.concat(cur))
+			.filter((item) =>
+				item.title.toLowerCase().includes(debouncedValue.toLowerCase())
+			);
+	}, [debouncedValue]);
+
 	return (
 		<section>
+			<PMSearch value={search} onChange={changeInputHandler} />
 			<PMTable>
 				<PMTableHead />
 				<PMTableBody pageLists={pageLists} offset={offset} />
