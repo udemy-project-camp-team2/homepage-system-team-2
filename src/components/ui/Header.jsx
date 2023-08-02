c1c0f7daecd4e122c32eac3fac11ffc2a44b7198
junseokoh-hub <lunulatecat@naver.com>
2023-08-02 10:40:53 +0900

    Feat: merge with login branch

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,54 +1,53 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import { Fragment } from 'react';
+import { useNavigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 import { styled } from 'styled-components';
 import { logout } from '../../store/slices/userSlice';
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router";
 import Logo from '../common/Logo';
 
 const HeaderStyled = styled.header`
-	display : flex;
+	display: flex;
 	width: 100%;
-	justify-content : center;
+	justify-content: center;
 `;
 const HeaderInner = styled.div`
-	width : 1220px;
+	width: 1220px;
 	height: 108px;
-	display : flex;
-	justify-content : center;
-	align-items : center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 const Btns = styled.div`
-	margin-left : auto;
+	margin-left: auto;
 `;
 
- const Btn = styled.button`
-    cursor: pointer;
-    display:inline-block;
-    padding: 0;
-    margin-left : 15px;
-    background : #fff;
-    border : none;
-    font-size: 12px;
-    color : #030303;
-    position: relative;
-    z-index : 1;
-    &::before {
-        display:block;
-        content : '';
-        position: absolute;
-        left : -8px;
-        top: 1px;
-        background : #B3B3B3;
-        width : 1px;
-        height : 12px;
-    }
-    &:first-child::before {
-      display: none;
-    }
- `;
+const Btn = styled.button`
+	cursor: pointer;
+	display: inline-block;
+	padding: 0;
+	margin-left: 15px;
+	background: #fff;
+	border: none;
+	font-size: 12px;
+	color: #030303;
+	position: relative;
+	z-index: 1;
+	&::before {
+		display: block;
+		content: '';
+		position: absolute;
+		left: -8px;
+		top: 1px;
+		background: #b3b3b3;
+		width: 1px;
+		height: 12px;
+	}
+	&:first-child::before {
+		display: none;
+	}
+`;
 
-const Header = ({children}) => {
+const Header = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const loginState = useSelector((state) => state.user.isLoggedIn);
@@ -56,33 +55,32 @@ const Header = ({children}) => {
 	const handleLogout = () => {
 		dispatch(logout());
 		navigate('/admin');
-	}
+	};
 	return (
 		<Fragment>
 			<HeaderStyled>
 				<HeaderInner>
-				<Logo/>
-				<Btns>
-					
-				{
-					loginState ?
-					<>
-					<Btn>마이페이지</Btn>
-					<Btn onClick={handleLogout}>로그아웃</Btn>
-					</>
-					:
-					<Btn onClick={()=>{navigate('/admin')}}>로그인</Btn>
-				}					
-				</Btns>
+					<Logo />
+					<Btns>
+						{loginState ? (
+							<>
+								<Btn>마이페이지</Btn>
+								<Btn onClick={handleLogout}>로그아웃</Btn>
+							</>
+						) : (
+							<Btn
+								onClick={() => {
+									navigate('/admin');
+								}}
+							>
+								로그인
+							</Btn>
+						)}
+					</Btns>
 				</HeaderInner>
 			</HeaderStyled>
 		</Fragment>
-		)
+	);
 };
 
 export default Header;
-
-
-Header.propTypes = {
-	children: PropTypes.node.isRequired,
-  };
