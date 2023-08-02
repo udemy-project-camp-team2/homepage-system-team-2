4f5251a16df54fda27ecab91839143887c16aae5
parkJi-yun <nmjk0123@naver.com>
2023-08-02 23:17:00 +0900

    type: Feat
    메뉴항목 추가 버튼 클릭하면 선택되어있는 토글메뉴안에 추가

--- src/components/management/MenuDetail.jsx
+++ src/components/management/MenuDetail.jsx
@@ -1,9 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronRight, faGear, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { toggleMenu, deleteItem, updateItem, addMenuItem } from '../../store/menu/menuReducer';
 import styled from 'styled-components';
-import { toggleMenu, deleteItem } from '../../store/menu/menuReducer';
-import { useState } from 'react';
 
 const MenuDetail = () => {
   const expandedMenu = useSelector(state => state.menu.expandedMenu);
@@ -11,37 +12,84 @@ const MenuDetail = () => {
   const [selectedItem, setSelectedItem] = useState(null);
   const dispatch = useDispatch();
 
+  // 메뉴 펼침/접기 토글 처리 함수
   const handleMenuToggle = (menu) => {
     dispatch(toggleMenu(menu));
   };
 
-  const handleItemDelete = (menu, title) => {
-    dispatch(deleteItem(menu, title));
+  // 항목 삭제 처리 함수
+  const handleItemDelete = (menu, id, title) => {
+    dispatch(deleteItem(menu, title, id));
     console.log(`${title} 삭제`);
   };
 
-  const handleItemDetails = (title, link) => {
+  // 항목 세부 정보 보기 처리 함수
+  const handleItemDetails = (id, title, link) => {
     setSelectedItem((prevSelectedItem) => {
-      if (prevSelectedItem && prevSelectedItem.title === title) {
-        return null;
+      if (prevSelectedItem && prevSelectedItem.id === id) {
+        return { ...prevSelectedItem };
       } else {
-        return { title, link };
+        return { id, title, link };
       }
     });
   };
 
+  // 항목 제목 변경 처리 함수
+  const handleTitleChange = (e) => {
+    setSelectedItem((prevSelectedItem) => ({
+      ...prevSelectedItem,
+      title: e.target.value,
+    }));
+  };
+
+  // 항목 링크 변경 처리 함수
+  const handleLinkChange = (e) => {
+    setSelectedItem((prevSelectedItem) => ({
+      ...prevSelectedItem,
+      link: e.target.value,
+    }));
+  };
+
+  // 항목 수정 확인 처리 함수
+  const handleItemUpdate = () => {
+    if (!selectedItem) return; // 수정 중인 아이템이 없으면 종료
+
+    const { id, title } = selectedItem;
+    const menu = Object.keys(currentMenuLists).find((menu) =>
+      currentMenuLists[menu].some((item) => item.id === selectedItem.id)
+    );
+
+    dispatch(updateItem(menu, id, title, selectedItem.title, selectedItem.link));
+    setSelectedItem(null);
+  };
+
+  // 메뉴 항목 추가 처리 함수
+  const handleMenuAdd = (menu) => {
+    const newItem = {
+      id: uuidv4(),
+      title: '새로운 항목',
+      link: '/new-link',
+    };
+
+    dispatch(addMenuItem(menu, newItem));
+    handleItemDetails(newItem.id, newItem.title, newItem.link);
+  };
+
+  console.log(selectedItem);
 
   return (
     <div>
       <h2>메뉴 설정</h2>
       <p>메뉴 항목과 구조를 설정해주세요.</p>
 
-      <button>메뉴 항목 추가</button>
+      {/* 메뉴 항목 추가 버튼 */}
+      <button onClick={() => handleMenuAdd(expandedMenu)}>메뉴 항목 추가</button>
 
       <div>
         <div>
           {Object.keys(currentMenuLists).map((menu) => (
-            <div key={menu} className="menu-item">
+            <div key={menu}>
+              {/* 메뉴 펼침/접기 아이콘 */}
               <MenuTitle onClick={() => handleMenuToggle(menu)}>
                 <FontAwesomeIcon
                   icon={expandedMenu === menu ? faChevronDown : faChevronRight}
@@ -50,32 +98,38 @@ const MenuDetail = () => {
               </MenuTitle>
               {expandedMenu === menu && (
                 <ul>
-                  {currentMenuLists[menu].map((item) => (
-                    <li key={item.title}>
+                  {currentMenuLists[menu]?.map((item) => (
+                    <li key={item.id}>
+                      {/* 항목 제목과 아이콘 */}
                       <ItemTitle>
                         {item.title}
                       </ItemTitle>
                       <IconWrapper>
+                        {/* 항목 세부 정보 보기 아이콘 */}
                         <FontAwesomeIcon
                           icon={faGear}
-                          onClick={() => handleItemDetails(item.title, item.link)}
+                          onClick={() => handleItemDetails(item.id, item.title, item.link)}
                         />
+                        {/* 항목 삭제 버튼 */}
                         <FontAwesomeIcon
                           icon={faTimes}
-                          onClick={() => handleItemDelete(menu, item.title)}
+                          onClick={() => handleItemDelete(menu, item.id, item.title)}
                         />
                       </IconWrapper>
-                      {selectedItem && selectedItem.title === item.title && (
+                      {/* 항목 수정 폼 */}
+                      {selectedItem && selectedItem.id === item.id && (
                         <ItemDetails>
                           <InputTitle
                             type="text"
                             value={selectedItem.title}
+                            onChange={handleTitleChange}
                           />
                           <InputLink
                             type="text"
                             value={selectedItem.link}
+                            onChange={handleLinkChange}
                           />
-                          <button>확인</button>
+                          <button onClick={handleItemUpdate}>확인</button>
                         </ItemDetails>
                       )}
                     </li>
--- src/store/menu/menuReducer.js
+++ src/store/menu/menuReducer.js
@@ -5,6 +5,14 @@ const initialState = {
   data: menuLists,
 };
 
+export const addMenuItem = (menu, newItem) => {
+  return {
+    type: 'ADD_MENU_ITEM',
+    menu,
+    newItem,
+  };
+};
+
 export const toggleMenu = (menu) => {
   return {
     type: 'TOGGLE_MENU',
@@ -20,22 +28,86 @@ export const deleteItem = (menu, title) => {
   };
 };
 
+export const updateItem = (menu, id, title, newTitle, newLink) => {
+  return {
+    type: 'UPDATE_ITEM',
+    menu,
+    id,
+    title,
+    newTitle,
+    newLink,
+  };
+};
 
 const menuReducer = (state = initialState, action) => {
   switch (action.type) {
+
     case 'TOGGLE_MENU':
+      // 토글 메뉴 액션 처리
       return {
         ...state,
         expandedMenu: state.expandedMenu === action.menu ? null : action.menu,
       };
+
     case 'DELETE_ITEM':
+      // 삭제 액션 처리
+      if (action.id && action.id.startsWith("temp-")) {
+        // 새로운 항목이면 DELETE_NEW_ITEM 액션을 디스패치하여 삭제
+        const menuData = state.data[action.menu].filter((item) => item.id !== action.id);
+        return {
+          ...state,
+          data: {
+            ...state.data,
+            [action.menu]: menuData,
+          },
+        };
+      } else {
+        // 기존 항목은 기존 방식으로 삭제
+        return {
+          ...state,
+          data: {
+            ...state.data,
+            [action.menu]: state.data[action.menu].filter((item) => item.title !== action.title),
+          },
+        };
+      }
+
+    case 'UPDATE_ITEM':
+      // 업데이트 액션 처리
       return {
         ...state,
         data: {
           ...state.data,
-          [action.menu]: state.data[action.menu].filter((item) => item.title !== action.title),
+          [action.menu]: state.data[action.menu].map((item) =>
+            item.id === action.id ? { ...item, title: action.newTitle, link: action.newLink } : item
+          ),
         },
       };
+
+    case 'ADD_MENU_ITEM':
+      // 메뉴 항목 추가 액션 처리
+      if (!state.data[action.menu]) {
+        // 해당 메뉴가 기존에 없으면 아무것도 변경하지 않음
+        return state;
+
+        // 새로운 MenuList 추가
+        // return {
+        //   ...state,
+        //   data: {
+        //     ...state.data,
+        //     [action.menu]: [action.newItem],
+        //   },
+        // };
+      }
+      // 해당 메뉴가 기존에 있으면 기존 데이터에 항목을 추가
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [action.menu]: [...state.data[action.menu], action.newItem],
+        },
+      };
+
     default:
       return state;
   }
