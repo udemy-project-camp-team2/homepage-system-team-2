7937b047b94c9728a352462a940a335e38e81c40
junseokoh-hub <lunulatecat@naver.com>
2023-08-06 14:22:35 +0900

    Feat: link to the admin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../components/common/Logo';
 
 const Home = () => {
-	return <Logo />;
+	return (
+		<Fragment>
+			<Logo />
+			<Link to="/admin">Admin</Link>
+		</Fragment>
+	);
 };
 
 export default Home;
