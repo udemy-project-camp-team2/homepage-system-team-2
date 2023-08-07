bd8b98bb9fc981b314202fa1720b0347c6f5a522
definitely92 <tngus5232@naver.com>
2023-08-07 15:02:22 +0900

    Design : Home Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../components/common/Logo';
+import styled from 'styled-components';
+
+const Header = styled.header`
+	width: 1170px;
+	margin: 0 auto;
+`;
 
 const Home = () => {
 	return (
 		<Fragment>
-			<Logo />
+			<Header>
+				<Logo />
+			</Header>
 			<Link to="/admin">Admin</Link>
 		</Fragment>
 	);
