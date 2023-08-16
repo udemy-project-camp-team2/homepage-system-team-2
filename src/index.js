78d7a5ef1a7bf03bde19cc0a1ee72875f4d3bc4a
junseokoh-hub <lunulatecat@naver.com>
2023-08-16 14:01:46 +0900

    Fix: extensions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -14,11 +13,9 @@ if (userFormLocalStorage) {
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-	// <StrictMode>
 	<StyledProvider>
 		<Provider store={store}>
 			<App />
 		</Provider>
 	</StyledProvider>
-	// </StrictMode>
 );
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,7 +2,6 @@ import { combineReducers } from '@reduxjs/toolkit';
 import containerSlice from './slices/containerSlice';
 import modalSlice from './slices/modalSlice';
 import userSlice from './slices/userSlice';
-import menuReducer from './menu/menuReducer';
 import selectedIdSlice from './slices/selectedIdSlice';
 import designSlice from './slices/designSlice';
 import menuSlice from './slices/menuSlice';
@@ -11,7 +10,6 @@ export const rootReducer = combineReducers({
 	containers: containerSlice,
 	modal: modalSlice,
 	user: userSlice,
-	// menu: menuReducer,
 	menu: menuSlice,
 	selectedId: selectedIdSlice,
 	design: designSlice,
